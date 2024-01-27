e90653056b787c49ddf873170501f9179fd946f0
kgarcia7 <kgarcia7814@gmail.com>
2024-01-27 14:52:20 -0500

    Updating app name

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,54 +1,54 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles. 
-      new HtmlWebpackPlugin({ 
-        template: './index.html',
-        title: 'J.A.T.E'
+      // Webpack plugin that generates our html file and injects our bundles.
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "J.A.T.E",
       }),
 
       // Injects our custom service worker
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
 
       // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'J.A.T.E. Logo',
-        short_name: 'Logo',
-        description: 'JATE logo',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "Text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
     ],
-// TODO: Add CSS loaders and babel to webpack.
+    // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
         {
@@ -59,15 +59,16 @@ module.exports = () => {
           test: /\.(?:js|mjs|cjs)$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
+              presets: [["@babel/preset-env"]],
+              plugins: [
+                "@babel/transform-runtime",
+                "@babel/plugin-proposal-object-rest-spread",
               ],
-              plugins: ['@babel/transform-runtime'],
-            }
-          }
-        }
+            },
+          },
+        },
       ],
     },
   };
